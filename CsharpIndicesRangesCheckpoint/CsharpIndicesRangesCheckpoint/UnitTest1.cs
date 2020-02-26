bebd43131d43d615599ac1968cc6820421d26307
Ashish Aggarwal <aggarwal.ash@husky.neu.edu>
2020-02-26 13:15:25 -0500

    Load Project

---
+++ CsharpIndicesRangesCheckpoint/CsharpIndicesRangesCheckpoint/UnitTest1.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsharpIndicesRangesCheckpoint
+{
+    [TestClass]
+    public class TestsBefore
+    {
+        readonly string[] GRADES = new string[]
+            {
+                "A", "B", "C", "D", "F"
+            };
+
+        [TestMethod]
+        public void GetGradeFromEnd_WhenCalledWithValidNumber_ShouldReturnGradeFromEnd()
+        {
+            string result = getGradeFromEnd(4);
+
+            Assert.AreEqual("B", result);
+        }
+
+        private string getGradeFromEnd(int indexFromEnd)
+        {
+            string item = GRADES[^indexFromEnd];
+            return item;
+        }
+
+        [TestMethod]
+        public void GetGradeRange_WhenCalledWithStartAndEndIndex_ShouldReturnGradeRange()
+        {
+            string[] result = getGradeRange(1, 3);
+
+            CollectionAssert.AreEqual(new string[] { "B", "C", "D" }, result);
+        }
+
+        private string[] getGradeRange(int startIndex, int endIndex)
+        {
+            //var length = endIndex - startIndex + 1;
+            var result = GRADES[startIndex..(endIndex+1)];
+            return result;
+        }
+    }
+}
